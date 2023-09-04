24739b8b96308c4b92b576c6ddebd743c9c0c033
Leandro Sonin <soninleandro@gmail.com>
2023-09-04 13:54:57 +0100

    add videos and 360

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,26 +1,55 @@
-/*Modal*/
+/*Modals*/
 
-var modal = document.getElementById("media-modal");
+/*Edificio*/
+
+var modalEdificio = document.getElementById("modal-edificio");
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("media-img")[0];
+var btnEdificio = document.getElementsByClassName("edificio-img")[0];
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanEdificio = document.getElementsByClassName("close-edificio")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btnEdificio.onclick = function() {
+  modalEdificio.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+spanEdificio.onclick = function() {
+  modalEdificio.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalEdificio) {
+    modalEdificio.style.display = "none";
+  }
+} 
+
+/*5A*/
+
+var modalDepto5a = document.getElementById("modal-5a");
+
+// Get the button that opens the modal
+var btnDepto5a = document.getElementsByClassName("5a-img")[0];
+
+// Get the <span> element that closes the modal
+var spanDepto5a = document.getElementsByClassName("close-5a")[0];
+
+// When the user clicks on the button, open the modal
+btnDepto5a.onclick = function() {
+  modalDepto5a.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanDepto5a.onclick = function() {
+  modalDepto5a.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalDepto5a) {
+    modalDepto5a.style.display = "none";
   }
 } 
