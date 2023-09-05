a32df7a17476534b1983a1603c8cd81fe72baf31
Leandro Sonin <soninleandro@gmail.com>
2023-09-05 11:29:47 +0100

    create mobile unidades section

--- assets/js/media.js
+++ assets/js/media.js
@@ -53,3 +53,57 @@ window.onclick = function(event) {
     modalDepto5a.style.display = "none";
   }
 } 
+
+/*3A*/
+
+var modalDepto3a = document.getElementById("modal-3a");
+
+// Get the button that opens the modal
+var btnDepto3a = document.getElementsByClassName("3a-img")[0];
+
+// Get the <span> element that closes the modal
+var spanDepto3a = document.getElementsByClassName("close-3a")[0];
+
+// When the user clicks on the button, open the modal
+btnDepto3a.onclick = function() {
+  modalDepto3a.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanDepto3a.onclick = function() {
+  modalDepto3a.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalDepto3a) {
+    modalDepto3a.style.display = "none";
+  }
+} 
+
+/*3B*/
+
+var modalDepto3b = document.getElementById("modal-3b");
+
+// Get the button that opens the modal
+var btnDepto3b = document.getElementsByClassName("3b-img")[0];
+
+// Get the <span> element that closes the modal
+var spanDepto3b = document.getElementsByClassName("close-3b")[0];
+
+// When the user clicks on the button, open the modal
+btnDepto3b.onclick = function() {
+  modalDepto3b.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanDepto3b.onclick = function() {
+  modalDepto3b.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalDepto3b) {
+    modalDepto3b.style.display = "none";
+  }
+} 
